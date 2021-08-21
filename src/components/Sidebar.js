33a17c2d9be7be3a4b536638b80996a3f6be750e
Oyunbileg Davaanyam <oyunbilegdavaanyam@Oyunbilegs-MacBook-Pro.local>
2021-08-21 03:24:01 -0700

    adworked on boardpage and set up routes in App.js

---
+++ src/components/Sidebar.js
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+    return (
+        <header className = 'bar'>
+            <button className = 'btn'>Go to Boards</button>
+        </header>
+    )
+}
+
+export default Sidebar;
---
+++ src/pages/home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <header>
+            <h2>Home Page</h2>
+        </header>
+    )
+}
+
+export default Home;
